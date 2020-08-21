4fc9b09603bea8bfe8355b2cdc22603d35b89de3
Jethro yu <jethroyu@163.com>
2020-08-21 16:50:56 +0800

    feat question1

---
+++ src/Solution/question1/Solution.java
@@ -0,0 +1,23 @@
+package Solution.question1;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++){
+            if (map.containsKey(target-nums[i])){
+                int[] ans = new int[2];
+                ans[0] = map.get(target-nums[i]);
+                ans[1] = i;
+                return ans;
+            }
+            else{
+                map.put(nums[i],i);
+            }
+        }
+        return new int[2];
+    }
+}
