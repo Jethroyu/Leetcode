0d606906217f6fd1cc8f2e6f87a8229c2653afb1
Jethro yu <jethroyu@163.com>
2020-09-14 15:07:16 +0800

    feat question143 双向队列模拟

---
+++ src/solution/question143/Solution.java
@@ -0,0 +1,38 @@
+package solution.question143;
+
+import template.ListNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @author shidu
+ * @date 2020/9/14 14:52
+ */
+class Solution {
+    public void reorderList(ListNode head) {
+        if (head == null) {
+            return;
+        }
+        LinkedList<ListNode> doubleQueue = new LinkedList<>();
+        ListNode ans = head;
+        while (ans!=null){
+            doubleQueue.add(ans);
+            ans = ans.next;
+        }
+        ans = head;
+        doubleQueue.pollFirst();
+        while (!doubleQueue.isEmpty()){
+            ans.next = doubleQueue.pollLast();
+            ans = ans.next;
+            ans.next = null;
+
+            if (!doubleQueue.isEmpty()) {
+                ans.next = doubleQueue.pollFirst();
+                ans = ans.next;
+                ans.next = null;
+            }
+        }
+        return;
+    }
+}
